8a0b771d3e2e60772e56c56963c87e37f622ebf7
Cameron <camlecuyer@yahoo.com>
2018-02-01 13:43:21 -0600

    Initialize

---
+++ Source/SmartShop/app/src/main/java/com/umkc/team11/smartshop/Home.java
@@ -0,0 +1,13 @@
+package com.umkc.team11.smartshop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
