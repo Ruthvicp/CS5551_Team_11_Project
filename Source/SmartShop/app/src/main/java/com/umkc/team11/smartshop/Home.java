dfbd9dedc712e45c44e0fb23ebb5702a300e054b
Cameron <camlecuyer@yahoo.com>
2018-02-23 14:49:10 -0600

    Basic searching

--- Source/SmartShop/app/src/main/java/com/umkc/team11/smartshop/Home.java
+++ Source/SmartShop/app/src/main/java/com/umkc/team11/smartshop/Home.java
@@ -1,7 +1,9 @@
 package com.umkc.team11.smartshop;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class Home extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+
+    public void toShopping(View v)
+    {
+        Intent redirect = new Intent(Home.this, ShoppingActivity.class);
+        startActivity(redirect);
     }
 }
---
+++ Source/SmartShop/app/src/main/java/com/umkc/team11/smartshop/ShoppingActivity.java
@@ -0,0 +1,41 @@
+package com.umkc.team11.smartshop;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+
+public class ShoppingActivity extends AppCompatActivity {
+
+    private SearchAPI search;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shopping);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        search = new SearchAPI();
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void searchCall(View v)
+    {
+        EditText searchText = findViewById(R.id.txt_search);
+        String searchPhrase = String.valueOf(searchText.getText());
+        search.searchAPIs(searchPhrase);
+    }
+
+}
