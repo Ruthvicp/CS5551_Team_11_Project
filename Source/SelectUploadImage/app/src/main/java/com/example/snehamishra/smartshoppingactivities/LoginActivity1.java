0d611d6bb6b9127bb746f652678a35c7407f332a
SnehaMishra <sneha.mishra.28@gmail.com>
2018-03-08 12:05:51 -0600

    Select & Upload Image to the Firebase Storage (Code & Document)

---
+++ Source/SelectUploadImage/app/src/main/java/com/example/snehamishra/smartshoppingactivities/LoginActivity1.java
@@ -0,0 +1,13 @@
+package com.example.snehamishra.smartshoppingactivities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class LoginActivity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
